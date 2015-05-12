115cb4952d3f90ffbbc427c4b8d36a0de60b2d84
Luke Klinker <lklinker1@gmail.com>
2015-05-12 18:39:56 -0500

    [Update] Make 'The Verge' my RSS feed

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@ var weatherParams = {
 };
 
 // Calendar can be changed from the calendar php file
-var feed = 'http://www.cbsnews.com/latest/rss/us';
+var feed = 'http://www.theverge.com/mobile/rss/index.xml';//http://www.cbsnews.com/latest/rss/us';
 var numCalendarEvents = 10;
 
 // compliments:
