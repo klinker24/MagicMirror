25086ca7c4076405af3047ae3966949b47e9bf5b
Luke Klinker <lklinker1@gmail.com>
2015-09-25 08:28:49 -0500

    use 20 calendar events

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var id = '4862034'; // default to iowa city
 var feed = 'http://www.cbsnews.com/latest/rss/us';
 
 // Calendar can be changed from the calendar php file
-var numCalendarEvents = 10;
+var numCalendarEvents = 20;
 
 // Notifications feed link can be found under main.js
 var numNotifications = 6;
